1c9325bbd3760f10d7fa1864e380ef69549bdaa7
Farid Amimri <farid.amimri@gmail.com>
2022-07-11 11:07:48 +0200

    Ajout textes à la Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,41 @@
+/** @format */
+
 import React from 'react'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import EmailIcon from '@mui/icons-material/Email'
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'>
+        <h2>Hi, My Name is Farid</h2>
+        <div className='prompt'>
+          <p>A software developer with a passion for learning and creating</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GitHubIcon />
+        </div>
+      </div>
+      <div className='skills'>
+        <h1>Skills</h1>
+        <ol className='list'>
+          <li className='item'>
+            <h2>Front-End</h2>
+            <span>
+              ReactJs, Redux, HTML, CSS, NPM, MaterialUI, StyledComponents
+            </span>
+          </li>
+          <li className='item'>
+            <h2>Languages</h2>
+            <span>
+              JavaScript
+            </span>
+          </li>
+        </ol>
+      </div>
+    </div>
   )
 }
 
