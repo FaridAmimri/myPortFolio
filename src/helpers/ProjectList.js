2aec059774be26709df8f9abcc74360d9ed3b846
Farid Amimri <farid.amimri@gmail.com>
2022-07-12 11:52:06 +0200

    Création array pour les projets

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,13 @@
+import Kasa from '../assets/kasa.jpg'
+import Sportsee from '../assets/sportsee.jpg'
+
+export const projectList = [
+    {
+        name: 'Kasa Application',
+        image: Kasa
+    },
+    {
+        name: 'SportSee Application',
+        image: Sportsee
+    }
+]
