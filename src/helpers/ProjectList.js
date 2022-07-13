83cae3dd011d0a3e83505bd4cdc698d753ecda04
Farid Amimri <farid.amimri@gmail.com>
2022-07-13 19:56:48 +0200

    Ajout projets à la liste

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,13 +1,41 @@
+/** @format */
+
+import FishEye from '../assets/fisheye.jpg'
+import LesPetitsPlats from '../assets/lespetitsplats.jpg'
 import Kasa from '../assets/kasa.jpg'
 import Sportsee from '../assets/sportsee.jpg'
+import ArgentBank from '../assets/argentbank.png'
+import HRnet from '../assets/hrnet.jpg'
 
 export const projectList = [
-    {
-        name: 'Kasa Application',
-        image: Kasa
-    },
-    {
-        name: 'SportSee Application',
-        image: Sportsee
-    }
+  {
+    name: 'FishEye',
+    image: FishEye,
+    skills: 'JavaScript, HTML, CSS'
+  },
+  {
+    name: 'LesPetitsPlats',
+    image: LesPetitsPlats,
+    skills: 'JavaScript, HTML, CSS'
+  },
+  {
+    name: 'Kasa',
+    image: Kasa,
+    skills: 'React, HTML, CSS'
+  },
+  {
+    name: 'SportSee',
+    image: Sportsee,
+    skills: 'React, HTML, CSS'
+  },
+  {
+    name: 'ArgentBank',
+    image: ArgentBank,
+    skills: 'React, Redux, HTML, CSS'
+  },
+  {
+    name: 'HRnet',
+    image: HRnet,
+    skills: 'React, Redux, HTML, CSS'
+  }
 ]
