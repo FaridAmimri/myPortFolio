dbb9cf48f28d08f9531dcdc94ba2fff751a2bfa6
Farid Amimri <farid.amimri@gmail.com>
2022-07-13 19:17:19 +0200

    Ajout path project/id

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import Experience from './pages/Experience'
 import Projects from './pages/Projects'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import ProjectDisplay from './pages/ProjectDisplay'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/projects' element={<Projects />} />
+          <Route path='/project/:id' element={<ProjectDisplay />} />
           <Route path='/experience' element={<Experience />} />
         </Routes>
         <Footer />
